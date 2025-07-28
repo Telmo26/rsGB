5f513eb704fb23803db24c9473b7c5e93a8d322d
Eliott tadros <53917558+Telmo26@users.noreply.github.com>
2025-07-28 13:22:47 +0200

    Added the processing for ADD, ADC, SUB and SBC

--- src/cpu/proc.rs
+++ src/cpu/proc.rs
@@ -20,6 +20,10 @@ impl CPU {
             InType::XOR => proc_xor(self, bus, ctx),
             InType::INC => proc_inc(self, bus, ctx),
             InType::DEC => proc_dec(self, bus, ctx),
+            InType::ADD => proc_add(self, bus, ctx),
+            InType::ADC => proc_adc(self, bus, ctx),
+            InType::SUB => proc_sub(self, bus, ctx),
+            InType::SBC => proc_sbc(self, bus, ctx),
             x => panic!("Instruction {x:?} not implemented")
         }
     }
@@ -194,4 +198,68 @@ fn proc_dec(cpu: &mut CPU, bus: &mut Interconnect, ctx: &mut EmuContext) {
     if !cpu.curr_inst.reg_1.is_16bit() {
         cpu.set_flags((val == 0) as u8, 1, ((val & 0x0F) == 0xF) as u8, BIT_IGNORE);
     }
+}
+
+fn proc_add(cpu: &mut CPU, _bus: &mut Interconnect, ctx: &mut EmuContext) {
+    let mut value = cpu.registers.read(cpu.curr_inst.reg_1).wrapping_add(cpu.fetched_data);
+
+    if cpu.curr_inst.reg_1.is_16bit() {
+        ctx.incr_cycle();
+    }
+
+    if cpu.curr_inst.reg_1 == RegType::SP {
+        let e = cpu.fetched_data as i8;
+        value = cpu.registers.read(cpu.curr_inst.reg_1).wrapping_add_signed(e as i16);
+    }
+
+    let mut z = (value == 0) as u8;
+    let mut h = ((cpu.registers.read(cpu.curr_inst.reg_1) & 0xF) + (cpu.fetched_data & 0xF)) > 0xF;
+    let mut c = ((cpu.registers.read(cpu.curr_inst.reg_1) & 0xFF) + (cpu.fetched_data & 0xFF)) > 0xFF;
+
+    if cpu.curr_inst.reg_1.is_16bit() && cpu.curr_inst.reg_1 != RegType::SP {
+        z = BIT_IGNORE;
+        h  = ((cpu.registers.read(cpu.curr_inst.reg_1) & 0xFFF) + (cpu.fetched_data & 0xFFF)) >= 0x10;
+        (_, c) = cpu.registers.read(cpu.curr_inst.reg_1).overflowing_add(cpu.fetched_data)
+    }
+
+    if cpu.curr_inst.reg_1 == RegType::SP {
+        z = 0;
+    }
+
+    cpu.registers.set(cpu.curr_inst.reg_1, value);
+    cpu.set_flags(z, 0, h as u8, c as u8);
+}
+
+fn proc_adc(cpu: &mut CPU, _bus: &mut Interconnect, _ctx: &mut EmuContext) {
+    let a = cpu.registers.a;
+    let c = cpu.c_flag() as u8;
+    let (sum, overflow1) = a.overflowing_add(cpu.fetched_data as u8);
+    let (sum, overflow2) = sum.overflowing_add(c);
+
+    cpu.registers.a = sum;
+    cpu.set_flags((sum == 0) as u8, 0, 
+        ((a & 0xF) + c + (cpu.fetched_data as u8 & 0xF) > 0xF) as u8,
+        (overflow1 || overflow2) as u8);
+}
+
+fn proc_sub(cpu: &mut CPU, _bus: &mut Interconnect, _ctx: &mut EmuContext) {
+    let (value, c) = cpu.registers.a.overflowing_sub(cpu.fetched_data as u8);
+    let h = cpu.fetched_data as u8 & 0xF > (cpu.registers.a & 0xF);
+
+    cpu.registers.a = value;
+    cpu.set_flags((value == 0) as u8, 1, h as u8, c as u8);
+}
+
+fn proc_sbc(cpu: &mut CPU, _bus: &mut Interconnect, _ctx: &mut EmuContext) {
+    let a = cpu.registers.a;
+    let u = cpu.fetched_data as u8;
+    let c = cpu.c_flag() as u8;
+
+    let (value, overflow1) = a.overflowing_sub(u);
+    let (value, overflow2) = value.overflowing_sub(c);
+
+    let h = (u + c) & 0xF > (a & 0xF);
+
+    cpu.registers.a = value;
+    cpu.set_flags((value == 0) as u8, 1, h as u8, (overflow1 || overflow2) as u8);
 }
