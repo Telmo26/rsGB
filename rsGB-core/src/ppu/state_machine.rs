4928630552a3598453612800d76846276361688f
Tadros Eliott <eliott.tadros@student-cs.fr>
2025-10-29 09:47:10 +0100

    Added the object fetching logic to the fetcher

--- rsGB-core/src/ppu/state_machine.rs
+++ rsGB-core/src/ppu/state_machine.rs
@@ -37,7 +37,7 @@ impl PPU {
             if ly >= LINES_PER_FRAME {
                 self.frame_complete();
                 change_lcd_mode(bus, LCDMode::OAM);
-                bus.write(0xFF44, 0);
+                lcd_write_ly(bus, 0);
             }
 
             self.line_ticks = 0;
